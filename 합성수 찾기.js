0faabbd48e0d53f11a5e7a626c9e48c80744c0e6
jaeuk Lee <lskkhj1005@naver.com>
2023-05-10 15:05:00 +0900

    합성수 찾기

---
+++ 합성수 찾기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  var answer = 0;
+
+  for (let i = 1; i <= n; i++) {
+    let num = 0;
+    for (let j = 0; j <= i; j++) {
+      if (i % j === 0) num++;
+    }
+    if (num > 2) {
+      answer += 1;
+    }
+  }
+
+  return answer;
+}
